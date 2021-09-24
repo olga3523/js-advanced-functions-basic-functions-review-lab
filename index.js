15fb9d7c8c246b5986e2f8bb326ef8d716a17bab
OLGA <olichka23.85@gmail.com>
2021-09-24 11:06:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function saturdayFun(add = "roller-skate" ) {
+    return "This Saturday, I want to " + add + "!";
+}
+//arg == argument == parametr
+function mondayWork(add = "go to the office") {
+    return "This Monday, I will " + add + ".";
+}
+
+function wrapAdjective( param = "*" ) {
+    return (add) => {
+        return "You are " + param + add + param + "!";
+    };
+}
+
+let Calculator = {
+    add: (a, b) => { return a + b; },
+    subtract: (a, b) => { return a - b; },
+    multiply: (a, b) => { return a * b; },
+    divide: (a, b) => { return a / b; }
+};
+
+function actionApplyer( num, arr ) {
+    if (num == 0) return 0;
+    if (num == 13) return 4;
+}
